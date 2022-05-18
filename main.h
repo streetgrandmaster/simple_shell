0735d36d252f84ca30cb8a1be7ae4269849e0c57
LAWRENCE <laurielawi12@gmail.com>
2022-05-18 21:59:05 +0300

    create main.h file

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdbool.h>
+
+#endif
