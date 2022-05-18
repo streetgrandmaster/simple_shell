8beb9a5f0ee387a8a36d7c16094b9744a291728f
LAWRENCE <laurielawi12@gmail.com>
2022-05-18 22:06:34 +0300

    create betty

--- main.h
+++ main.h
@@ -12,4 +12,58 @@
 #include <time.h>
 #include <stdbool.h>
 
+
+extern char **environ;
+extern __sighandler_t signal(int __sig, __sighandler_t __handler);
+
+void exit_cmd(char **command, char *line);
+
+
+/* handle built ins */
+int checker(char **cmd, char *buf);
+void prompt_user(void);
+int handle_builtin(char **command, char *line);
+void exit_cmd(char **command, char *line);
+void execution(char *cp, char **cmd);
+char *append_path(char *path, char *command);
+void handle_signal(int m);
+void handle_signal(int m);
+char **tokenizer(char *line);
+char *test_path(char **path, char *command);
+char *append_path(char *path, char *command);
+int handle_builtin(char **command, char *line);
+void exit_cmd(char **command, char *line);
+
+void print_env(void);
+
+/* string handlers */
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+int _strncmp(char *s1, char *s2, int n);
+char *_strdup(char *s);
+char *_strchr(char *s, char c);
+
+void execution(char *cp, char **cmd);
+char *find_path(void);
+
+/* helper function for efficient free */
+void free_buffers(char **buf);
+
+struct builtin
+{
+    char *env;
+    char *exit;
+} builtin;
+
+struct info
+{
+    int final_exit;
+    int ln_count;
+} info;
+
+struct flags
+{
+    bool interactive;
+} flags;
+
 #endif
